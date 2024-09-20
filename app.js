c280d8e2c2292327669b3824a7787add5267318a
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-09-20 13:35:39 +0530

    module.exports and require

---
+++ app.js
@@ -0,0 +1,25 @@
+
+require("./abc");    //used as import we used in react there is no need to write export for just console.log
+const calculateSum=require("./sum"); //for variables and function we import like this
+//var name="Kaustubh Sawant";
+//other file is also known as module
+var a=10;
+
+var b=20;
+
+calculateSum(a,b);
+
+
+// console.log(name);
+ //console.log(a+b);
+
+// console.log(global);
+
+// console.log(globalThis);  //universal global object
+
+// console.log(globalThis === global); //true
+
+
+
+
+
