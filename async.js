7ad077a9b169b0f7177b0753604a58d91ed19faf
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-09-24 12:41:52 +0530

    sync and async code

---
+++ async.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+const https = require("https");
+
+console.log("Hello World");
+
+var a = 1078698;
+var b = 20986;
+
+https.get("https://dummyjson.com/products/1" , (res) => {
+    console.log("Fetched Data Succesfully");
+    
+});
+
+setTimeout(()=>{
+    console.log("set Timeout called afer 5 seconds");
+    
+},5000);
+
+fs.readFile("./file.txt", "utf8", (err,data) =>{
+console.log("File Data :",data);
+
+});
+
+function multiplyFn(a,b){
+    const result =a + b;
+    return result;
+}
+
+
+var c=multiplyFn(a,b);
+
+console.log("Multiplication result is :" ,c);
+
+
+
+//1st hello world and synchronous operations will be printed
+//then async operations will be printed
+//in async operations 1st file read write then second timeout will happen after 5 seconds and then api data is fetched at last
+
